b58fb6738d5f177738c9c511a0742b6fd22a23af
cuongyd196 <cuongyd196@gmail.com>
2021-10-29 14:36:17 +0700

    #commit

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import  mongoose from 'mongoose';
-import {todoRouter} from './src/todo.router';
+import {todoRouter} from './api/todo/todo.router';
 
 const  PORT = 4000;
 const  app = express();
 app.use( cors());
 app.use(bodyParser.json());
 app.use('/api/todos', todoRouter);
-mongoose.connect('mongodb://localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=todolist');
+mongoose.connect('mongodb+srv://Cuongyd196:1@cluster0.q4glj.mongodb.net/db_todolist?retryWrites=true&w=majority');
 
 mongoose.connection.once('open', function () {
     console.log("Connection mongodb success");
